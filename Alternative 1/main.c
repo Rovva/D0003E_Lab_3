164e8fc9909e3faadc12d1b98bb4842e483711dc
Rovva <rovva@hostarn.se>
2022-01-28 20:32:05 +0100

    Init Alternative 1 windows

---
+++ Alternative 1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Alternative 1.c
+ *
+ * Created: 2022-01-28 20:31:39
+ * Author : chris
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
